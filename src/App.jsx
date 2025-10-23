6174ebc28785fe8dd90e57ea2ae3e3b43a61002f
chan8632 <duscks8632@gmail.com>
2025-10-23 11:08:13 +0900

    feat: 현재 위치 기반 날씨 정보 표시 기능 구현
    navigator.geolocation을 사용하여 사용자의 현재 위치(위도, 경도)를 가져옵니다. 가져온 위치를 기반으로 OpenWeatherMap API를 호출하여 날씨 데이터를 fetch합니다. 데이터를 받아오는 동안 로딩 상태를 표시하고, 완료되면 시간대, 현재 온도(섭씨/화씨), 날씨 설명을 렌더링합니다.
    
    부가적으로 main.jsx에서 StrictMode를 제거합니다.
    2-4 위치기반 날씨정보 불러오기 (내가함)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
 
 function App() {
-  return(<div>dd</div>)
+  const [json, setJson] = useState("");
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    getCurPosWeatherJson();
+  }, []);
+  const getCurPosWeatherJson = () => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      const response = await fetch(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=d909c7f791a049ea70613e9382509195`
+      );
+      const json = await response.json();
+      console.log(json);
+      setLoading(false);
+      setJson(json);
+    });
+  };
 
+  return loading ? (
+    "loading"
+  ) : (
+    <div>
+      <div className="timezone">{json.timezone}</div>
+      <div className="c">{json.current.temp}</div>
+        <div className="f">{((json.current.temp)*9/5)+32}</div>
+      <div className="description">{json.current.weather[0].description}</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
